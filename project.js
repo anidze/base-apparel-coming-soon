8f789e601447e7c9f89cc52a4e2fbda50f5cd6b9
ano gambashidze <ano.gambashidze@gmail.com>
2023-05-15 01:53:13 +0400

    base apparel project desktop version

--- project.js
+++ project.js
@@ -1,2 +1,15 @@
+let fname = document.getElementById("fname");
 
+let formAc = document.getElementById("formAc");
 
+function validateEmail() {
+  if (!fname.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    let arrpeImg = document.getElementById("errorImg");
+    arrpeImg.style.display = "block";
+    let errorspan = document.getElementById("errorspan");
+    errorspan.style.display = "block";
+  } else {
+    let respond = document.getElementById("respond");
+    respond.style.display = "block";
+  }
+}
